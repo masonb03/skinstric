a7e08b4d6bf30dd2a8cc8e74814fd05bee7bbea9
Mason <bevinsm355@gmail.com>
2026-05-01 22:03:27 -0400

    finished layout for the result page, and started the route to the camera page

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -2,7 +2,7 @@
 import IntroStep from '@/components/IntroStep';
 
 const page = () => {
-  return <IntroStep direction="Click to type" placeholder="your city name" nextRoute="next-step" field="location" isLastStep={true}/>
+  return <IntroStep direction="Click to type" placeholder="your city name" nextRoute="result" field="location" isLastStep={true}/>
 }
 
 export default page
